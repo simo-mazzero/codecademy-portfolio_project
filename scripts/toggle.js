a42f64138b5ee99b8b9235f89be631d37649b08a
simo-mazzero <simone.mazzero@gmail.com>
2023-05-01 16:14:33 +0200

    Project last touches

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -2,7 +2,9 @@ function toggleNav() {
     var x = document.getElementById("toggle-nav");
     if (x.style.display === "block") {
         x.style.display = "none";
+        document.getElementById("icon").className = "fa fa-bars";
     } else {
         x.style.display = "block";
+        document.getElementById("icon").className = "fa fa-xmark";
     }
 }
